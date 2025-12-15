ca321332d9da1face5f6d8b9accaa1230f081e83
xiaoyan <xiaoyan@192.168.0.106>
2025-12-15 21:55:49 +0800

    update dataset_partitioning

--- Dataset_partitioning.py
+++ Dataset_partitioning.py
@@ -11,7 +11,8 @@ classes = ['airplane', 'airport', 'baseballfield', 'basketballcourt', 'bridge', 
            'overpass', 'ship', 'stadium', 'storagetank', 'tenniscourt', 'trainstation', 'vehicle', 'windmill']
 
 
-def seg_img_type(xml_path, txt_path, train_val_percent=0.9, train_percent=0.7, xml_deep: str = None):
+def seg_img_type(xml_path, txt_path, source_images_path, train_val_percent=0.9, train_percent=0.7,
+                 xml_deep: str = None):
     """
     XML文件按照一定的比例分成train、val、test三个数据集
     train_val_percent  表示的是train+val之和。若不需要test集则改为1
@@ -22,10 +23,14 @@ def seg_img_type(xml_path, txt_path, train_val_percent=0.9, train_percent=0.7, x
         xml_path = os.path.join(xml_path, xml_deep)
     txt_path = os.path.join(txt_path, 'seg')
     total_xml = os.listdir(xml_path)
-
+    xml_num_list = [i.split('.')[0] for i in total_xml]
+    source_jpg_path = os.path.join(xml_path, source_images_path)
+    jpg_list = os.listdir(source_jpg_path)
+    jpg_num_list = [i.split('.')[0] for i in jpg_list]
+    xml_exists_list = [i for i in total_xml if i in jpg_num_list]
     if not os.path.exists(txt_path):
         os.makedirs(txt_path)
-    num = len(total_xml)
+    num = len(xml_exists_list)
     list_index = range(num)
     tv = int(num * train_val_percent)
     tr = int(tv * train_percent)
@@ -106,7 +111,7 @@ def copy_img(source_jpg_path, img_path, image_id):
     shutil.copyfile(os.path.join(source_jpg_path, f'{image_id}.jpg'), os.path.join(img_path, f'{image_id}.jpg'))
 
 
-def xml2txt(source_path, out_path,source_images_path, xml_deep: str = None, is_difficult=True):
+def xml2txt(source_path, out_path, source_images_path, xml_deep: str = None, is_difficult=True):
     seg_path = os.path.join(out_path, 'seg')
     img_path = os.path.join(out_path, 'images')
     source_jpg_path = os.path.join(source_path, source_images_path)
@@ -128,12 +133,13 @@ def xml2txt(source_path, out_path,source_images_path, xml_deep: str = None, is_d
         list_file.close()
 
 
-def split_dataset(source, output,source_images_path, xml_deep, is_difficult=True):
-    seg_img_type(source, output, train_val_percent=1, xml_deep=xml_deep)
+def split_dataset(source, output, source_images_path, xml_deep, is_difficult=True):
+    seg_img_type(source, output, source_images_path, train_val_percent=1, xml_deep=xml_deep)
     xml2txt(source, output, source_images_path, xml_deep, is_difficult)
 
 
 if __name__ == '__main__':
     source_dir = '/home/yjj/data/OpenDataLab___DIOR/raw/DIOR'
     output_dir = 'Datasets'
-    split_dataset(source_dir, output_dir, "JPEGImages-trainval", xml_deep='Horizontal Bounding Boxes', is_difficult=False)
+    split_dataset(source_dir, output_dir, "JPEGImages-trainval", xml_deep='Horizontal Bounding Boxes',
+                  is_difficult=False)
